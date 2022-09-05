a9ad1a6858b44dff7716fd129aa6f366612321c2
= <=>
2022-09-05 10:13:14 -0700

    Initial strip of starter template to prep for auth

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
+// import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>This will display after login</h1>
+<!--    <TheWelcome />-->
   </main>
 </template>
