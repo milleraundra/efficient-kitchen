0d63de8ba9133862afdc5abcb3739c618670f2ec
= <=>
2022-09-05 18:13:31 -0700

    Add enough firebase configs to begin testing

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,32 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { initializeApp } from 'firebase/app';
+import { getAuth } from "firebase/auth";
+
+
+// Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyA-6XjrdGqUkYCKuC6yGJzVHK_ZTvDY960",
+    authDomain: "efficient-kitchen.firebaseapp.com",
+    projectId: "efficient-kitchen",
+    storageBucket: "efficient-kitchen.appspot.com",
+    messagingSenderId: "519643401307",
+    appId: "1:519643401307:web:738c0f01e01845e5bb037e"
+};
 
 const app = createApp(App)
 
-app.use(router)
-
+app.use(router);
 app.mount('#app')
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,6 +1,37 @@
+<script lang="ts">
+import {getAuth, createUserWithEmailAndPassword} from "firebase/auth";
+
+export default {
+
+  methods: {
+    login() {
+      const email = '';
+      const password = '';
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth, email, password)
+          .then((userCredential) => {
+            // Signed in
+            console.log(userCredential);
+            const user = userCredential.user;
+            // ...
+          })
+          .catch((error) => {
+            console.log(error);
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+          });
+
+    }
+  }
+
+}
+</script>
+
 <template>
   <div class="about">
     <h1>This is where login will happen</h1>
+    <button @click="login">Log In</button>
   </div>
 </template>
 
